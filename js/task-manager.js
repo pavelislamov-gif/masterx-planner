aa40d3b1806d0fedbd22a6eaf46bcf461f9eb42f
pavelislamov-gif <pavel.islamov@gmail.com>
2026-03-16 16:43:54 +0700

    Update task-manager.js

--- js/task-manager.js
+++ js/task-manager.js
@@ -12,6 +12,7 @@ class TaskManager {
     // Загрузить базу операций из техкарт
     loadOperationsDatabase() {
         return {
+            // ТОКАРНЫЙ УЧАСТОК
             'tokarniy': {
                 'XRAY 1': ['Заготовка', 'Точение Корпуса', 'Заготовка деталей Крышка', 'Точение деталей Кольцо', 'Фрезеровка Корпуса'],
                 'XRAY 3': ['Заготовка', 'Точение Корпуса', 'Заготовка деталей Крышка', 'Точение деталей Кольцо', 'Фрезеровка Корпуса'],
@@ -28,7 +29,7 @@ class TaskManager {
                 'XRAY 6-T2 BT 240 Шторка': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
                 'XRAY 6-T2 BZ 180': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
                 'XRAY 6-T2 BZ 200 Шторка': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
-                'XRAY 6-T2 BZ 220': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
+                'XRAY 6-T2 BZ 220': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса', 'Фрезеровка Кронштейна'],
                 'XRAY 6-T2 BZ 220 Шторка x2': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
                 'XRAY 6-T2 BZ 240 Шторка': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
                 'XRAY 6T Накладной': ['Заготовка', 'Точение Корпуса', 'Точение Крепления', 'Фрезеровка Корпуса', 'Фрезеровка крепления'],
@@ -56,6 +57,8 @@ class TaskManager {
                 'XGRAY v.2': ['Заготовка', 'Точение Корпуса', 'Фрезеровка Корпуса'],
                 'XSMART mini': ['Заготовка', 'Точение']
             },
+            
+            // СЛЕСАРНЫЙ УЧАСТОК
             'slesarniy': {
                 'XRAY 1': ['Нарезка резьбы Корпус+V', 'Нарезка резьбы Крышка+V', 'Голтовка Кронштейна', 'УВ корпуса'],
                 'XRAY 3': ['Нарезка резьбы Корпус+V', 'Нарезка резьбы Крышка+V', 'Голтовка Кронштейна', 'РТИ', 'УВ корпуса'],
@@ -69,101 +72,30 @@ class TaskManager {
                 'XRAY 6-T2 BT 200': ['Нарезка резьбы Корпус+V', 'Установка Резьбовых заклепок 4*16', 'Нарезка резьбы Основание платы+V', 'Торцовка Корпуса под шторку', 'Обработка Корпуса', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
                 'XRAY 6-T2 BT 220': ['Нарезка резьбы Корпус+V', 'Установка Резьбовых заклепок 4*16', 'Нарезка резьбы Основание платы+V', 'Обработка Корпуса', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
                 'XRAY 6-T2 BT 220 Шторка x2': ['Нарезка резьбы Корпус+V', 'Установка Резьбовых заклепок 4*16', 'Нарезка резьбы Основание платы+V', 'Торцовка Корпуса под шторку', 'Обработка Корпуса', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
-                'XRAY 6-T2 BT 240 Шторка': ['Нарезка резьбы Корпус+V', 'Установка Резьбовых заклепок 4*16', 'Нарезка резьбы Основание платы+V', 'Торцовка Корпуса под шторку', 'Обработка Корпуса', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
                 'XRAY 6T Накладной': ['Нарезка резьбы Крышка+V', 'Нарезка резьбы Крепление', 'Нарезка резьбы Основание платы+V', 'Обработка Корпуса', 'Обработка Основания платы', 'УВ корпуса'],
                 'XRAY 9': ['Нарезка резьбы Корпус+V', 'Нарезка резьбы Крышка+V', 'Кольцо V', 'Голтовка Кронштейна', 'УВ корпуса'],
-                'XRAY 9S': ['Нарезка резьбы Корпус+V', 'Нарезка резьбы Основание платы+V', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
-                'XRAY 12S': ['Нарезка резьбы Корпус+V', 'Нарезка резьбы Основание платы+V', 'Обработка Основания платы', 'Голтовка Кронштейна', 'УВ корпуса'],
                 'XGRAY v.1': ['Нарезка резьбы', 'Голтовка', 'УВ корпуса'],
-                'XGRAY v.2': ['Нарезка резьбы', 'Голтовка', 'УВ корпуса'],
                 'XSMART mini': ['Сборка']
             },
+            
+            // ФРЕЗЕРНЫЙ УЧАСТОК
             'frezerniy': {
                 'XRAY 1': ['Поликарбонат Прозрачный 6мм'],
-                'XRAY 3': ['Поликарбонат Прозрачный 6мм'],
-                'XRAY 3-2': ['Поликарбонат Прозрачный 6мм х 2'],
-                'XRAY 6-2 проходной': ['Поликарбонат Прозрачный 3мм х 2'],
-                'XRAY 6-2 оконечный': ['Поликарбонат Прозрачный 3мм х 2'],
-                'XRAY 9': ['Поликарбонат Прозрачный 6мм'],
-                'XRAY 18': ['Поликарбонат Прозрачный 6мм'],
-                'XRAY 36': ['Поликарбонат Прозрачный 6мм'],
-                'XPIXEL BIN v.1': ['Plexiglas Опал 30% 2мм'],
-                'XPIXEL BIN v.2': ['Plexiglas Опал 30% 2мм'],
-                'XPIXEL BIN v.3': ['Plexiglas Опал 30% 2мм'],
-                'XDISK': ['ПВХ 10мм'],
-                'XROLL-lite P': ['Заглушка AL 3мм'],
-                'XROLL-lite K': ['Заглушка AL 3мм'],
-                'XWHITE': ['Заглушка AL 3мм'],
-                'XEYES 130*90 1': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*90 2': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*90 3': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*90 4': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*120 1': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*120 2': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*120 3': ['Поликарбонат Прозрачный 3мм'],
-                'XEYES 130*120 4': ['Поликарбонат Прозрачный 3мм'],
-                'XGIRO': ['Заглушка AL 3мм', 'Вставка ПВХ 3мм'],
-                'XGLOW': ['Заглушка AL 3мм Левая', 'Заглушка AL 3мм Правая'],
                 'XGRAY v.1': ['Заглушка AL 3мм Левая', 'Заглушка AL 3мм Правая'],
-                'XGRAY v.2': ['Заглушка AL 3мм Левая', 'Заглушка AL 3мм Правая'],
-                'XLITE': ['Вставка ПВХ 3мм', 'Заглушка AL 3мм'],
-                'XSMART': ['Вставка ПВХ 3мм', 'Заглушка AL 3мм'],
-                'XSMART MINI': ['Вставка ПВХ 3мм', 'Заглушка AL 3мм'],
-                'XSTRONG': ['Вставка ПВХ 3мм'],
-                'XBAR-SW': ['Вставка ПВХ 3мм'],
-                'XFOCUS': ['Вставка ПВХ 3мм'],
-                'XYELLOW': ['Заглушка AL 3мм'],
-                'XLINE': ['Заглушка AL 4мм']
+                'XLUMO': ['Заглушка AL 3мм', 'Вставка ПВХ 3мм без выемки верхняя', 'Вставка ПВХ 3мм без выемки нижняя', 'Вставка ПВХ 3мм с выемкой верхняя', 'Вставка ПВХ 3мм с выемкой нижняя']
             },
+            
+            // ЛАЗЕРНО-ГИБОЧНЫЙ УЧАСТОК
             'lazerno': {
                 'XRAY 1': ['Раскрой XRAY 1 Кронштейн', 'Гибка XRAY 1 Кронштейн'],
-                'XRAY 3': ['Раскрой Кронштейн A', 'Гибка Кронштейн A'],
-                'XRAY 3-2': ['Раскрой Кронштейн A', 'Гибка Кронштейн A'],
-                'XRAY 3-GRP': ['Раскрой XRAY 1 Кронштейн', 'Гибка XRAY 1 Кронштейн'],
-                'XRAY 6': ['Раскрой Фоновая заглушка', 'Раскрой Кронштейн B', 'Гибка Кронштейн B'],
-                'XRAY 6 RGBW': ['Раскрой Фоновая заглушка', 'Раскрой Кронштейн B', 'Гибка Кронштейн B'],
-                'XRAY 6-2 проходной': ['Раскрой Кронштейн B', 'Гибка Кронштейн B'],
-                'XRAY 6-2 оконечный': ['Раскрой Кронштейн B', 'Гибка Кронштейн B'],
-                'XRAY 6-T2 BT 180': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн BT', 'Гибка Кронштейн BT'],
-                'XRAY 6-T2 BT 200': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн BT', 'Гибка Кронштейн BT'],
-                'XRAY 6-T2 BT 220': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн BT', 'Гибка Кронштейн BT'],
-                'XRAY 6-T2 BT 220 Шторка x2': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн BT', 'Гибка Кронштейн BT'],
-                'XRAY 6-T2 BT 240 Шторка': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн BT', 'Гибка Кронштейн BT'],
-                'XRAY 6T Накладной': ['Раскрой Задняя крышка', 'Раскрой Основания платы', 'Раскрой Фоновая заглушка'],
-                'XRAY 9': ['Раскрой Кронштейн C', 'Гибка Кронштейн C'],
-                'XRAY 9S': ['Раскрой Задняя крышка', 'Раскрой Основания платы', 'Раскрой Кронштейн C', 'Гибка Кронштейн C'],
-                'XRAY 12S': ['Раскрой Задняя крышка', 'Раскрой Основания платы', 'Раскрой Кронштейн F', 'Гибка Кронштейн F'],
-                'XRAY 18': ['Раскрой Фоновая заглушка', 'Раскрой Кронштейн D', 'Гибка Кронштейн D'],
-                'XRAY 18S': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн D', 'Гибка Кронштейн D'],
-                'XRAY 36': ['Раскрой Фоновая заглушка', 'Раскрой Кронштейн G', 'Гибка Кронштейн G'],
-                'XRAY 36S': ['Раскрой Основания платы', 'Раскрой Фоновая заглушка', 'Раскрой Кронштейн G', 'Гибка Кронштейн G'],
                 'XGRAY v.1': ['Раскрой Заглушка модуля'],
-                'XGRAY v.2': ['Раскрой Заглушка модуля', 'Раскрой Кронштейна P Лира выносная', 'Раскрой Кронштейн LU или PU', 'Гибка Кронштейна P Лира выносная', 'Гибка Кронштейн LU или PU'],
-                'XSMART mini': ['Раскрой Вставка XSMART mini', 'Раскрой Кронштейн P Лира', 'Раскрой Кронштейн LU или PU', 'Гибка Кронштейн P Лира', 'Гибка Кронштейн LU или PU'],
-                'XLITE': ['Раскрой Лира (Q-серия) Универсальная', 'Раскрой Кронштейн LU или PU', 'Гибка Лира (Q-серия) Универсальная', 'Гибка Кронштейн LU или PU']
+                'XGRAY v.2': ['Раскрой Заглушка модуля', 'Раскрой Кронштейна P Лира выносная', 'Раскрой Кронштейн LU или PU', 'Гибка Кронштейна P Лира выносная', 'Гибка Кронштейн LU или PU']
             },
+            
+            // ПОЛИМЕРНЫЙ УЧАСТОК
             'polimerniy': {
                 'XRAY 1': ['Корпус в сборе', 'Кронштейн'],
-                'XRAY 3': ['Корпус в сборе', 'Кронштейн'],
-                'XRAY 3-2': ['Корпус в сборе', 'Кронштейн'],
-                'XRAY 3-GRP': ['Корпус', 'Кронштейн'],
-                'XRAY 6': ['Корпус', 'Кронштейн', 'Фоновая Заглушка'],
-                'XRAY 6 RGBW': ['Корпус', 'Кронштейн', 'Фоновая Заглушка'],
-                'XRAY 6-2 проходной': ['Корпус в сборе', 'Кронштейн'],
-                'XRAY 6-2 оконечный': ['Корпус в сборе', 'Кронштейн'],
-                'XRAY 6-T2 BT 180': ['Корпус', 'Фоновая заглушка'],
-                'XRAY 6-T2 BT 200': ['Корпус', 'Фоновая заглушка'],
-                'XRAY 6-T2 BT 220': ['Корпус', 'Фоновая заглушка'],
-                'XRAY 6-T2 BT 220 Шторка x2': ['Корпус', 'Фоновая заглушка'],
-                'XRAY 6-T2 BT 240 Шторка': ['Корпус', 'Фоновая заглушка'],
-                'XRAY 6T Накладной': ['Корпус', 'Крепление', 'Задняя крышка', 'Фоновая заглушка'],
-                'XRAY 9': ['Корпус', 'Кольцо', 'Задняя крышка', 'Кронштейн'],
-                'XRAY 9S': ['Корпус', 'Фоновая заглушка', 'Кронштейн'],
-                'XRAY 12S': ['Корпус', 'Фоновая заглушка', 'Кронштейн'],
-                'XRAY 18': ['Корпус', 'Фоновая заглушка', 'Кольцо', 'Задняя крышка', 'Кронштейн'],
-                'XRAY 18S': ['Корпус', 'Фоновая заглушка', 'Кронштейн'],
                 'XGRAY v.1': ['Профиль', 'Заглушка Левая', 'Заглушка Правая', 'Кронштейн AL'],
-                'XGRAY v.2': ['Профиль', 'Заглушка Левая', 'Заглушка Правая', 'Лира', 'Кронштейн'],
                 'XSMART mini': ['Профиль', 'Заглушка', 'Лира', 'Кронштейн']
             }
         };
@@ -200,8 +132,19 @@ class TaskManager {
         const savedHistory = localStorage.getItem(`tasks_${this.siteType}_${dateStr}`);
         
         if (savedHistory) {
-            this.taskHistory[dateStr] = JSON.parse(savedHistory);
-            this.tasks = this.taskHistory[dateStr] || [];
+            let historyTasks = JSON.parse(savedHistory);
+            
+            // Фильтруем задачи: оставляем только те, чьи заказы существуют
+            historyTasks = historyTasks.filter(task => {
+                return this.orders.some(o => o.id === task.orderId);
+            });
+            
+            this.taskHistory[dateStr] = historyTasks;
+            this.tasks = historyTasks;
+            
+            if (historyTasks.length !== JSON.parse(savedHistory).length) {
+                this.saveHistoryForDate(date);
+            }
         } else {
             this.tasks = this.generateTasks();
             this.saveHistoryForDate(date);
@@ -222,19 +165,33 @@ class TaskManager {
         const tasks = [];
         const dateStr = this.formatDate(this.currentDate);
         
-        this.orders.forEach(order => {
-            if (order.status !== 'active') return;
-            
+        const activeOrders = this.orders.filter(o => o.status === 'active');
+        
+        activeOrders.forEach(order => {
             order.items.forEach(item => {
                 const productName = item.product;
                 const operations = this.getOperationsForProduct(productName);
+                const totalQuantity = item.quantity || 1;
                 
                 operations.forEach((op, index) => {
                     const taskId = `${order.id}_${productName}_${this.siteType}_${index}`;
-                    const existingTask = this.taskHistory[dateStr]?.find(t => t.id === taskId);
+                    
+                    // Ищем существующую задачу в истории
+                    let existingTask = null;
+                    for (let date in this.taskHistory) {
+                        const found = this.taskHistory[date].find(t => t.id === taskId);
+                        if (found) {
+                            existingTask = found;
+                            break;
+                        }
+                    }
                     
                     if (existingTask) {
-                        tasks.push(existingTask);
+                        // Переносим задачу на текущую дату с сохранением прогресса
+                        tasks.push({
+                            ...existingTask,
+                            date: dateStr
+                        });
                     } else {
                         tasks.push({
                             id: taskId,
@@ -242,7 +199,7 @@ class TaskManager {
                             orderNumber: order.number,
                             product: productName,
                             size: item.size || 'Стандартный',
-                            totalQuantity: item.quantity || 1,
+                            totalQuantity: totalQuantity,
                             completedQuantity: 0,
                             operation: op,
                             operationIndex: index,
@@ -255,15 +212,27 @@ class TaskManager {
                 });
             });
             
+            // Дополнительные задачи
             if (order.extraTasks && order.extraTasks.length > 0) {
                 order.extraTasks.forEach((extraTask, index) => {
                     if (extraTask.site !== this.siteType) return;
                     
                     const taskId = `${order.id}_extra_${index}`;
-                    const existingTask = this.taskHistory[dateStr]?.find(t => t.id === taskId);
+                    
+                    let existingTask = null;
+                    for (let date in this.taskHistory) {
+                        const found = this.taskHistory[date].find(t => t.id === taskId);
+                        if (found) {
+                            existingTask = found;
+                            break;
+                        }
+                    }
                     
                     if (existingTask) {
-                        tasks.push(existingTask);
+                        tasks.push({
+                            ...existingTask,
+                            date: dateStr
+                        });
                     } else {
                         tasks.push({
                             id: taskId,
@@ -323,9 +292,7 @@ class TaskManager {
     
     // Добавить исполнителя
     addExecutor(taskId, executorName) {
-        const dateStr = this.formatDate(this.currentDate);
         const taskIndex = this.tasks.findIndex(t => t.id === taskId);
-        
         if (taskIndex === -1) return false;
         
         if (!this.tasks[taskIndex].executors) {
@@ -343,7 +310,7 @@ class TaskManager {
         return true;
     }
     
-    // Обновить количество и статус исполнителя
+    // Обновить количество исполнителя
     updateExecutorQuantity(taskId, executorName, quantity) {
         const taskIndex = this.tasks.findIndex(t => t.id === taskId);
         if (taskIndex === -1) return false;
@@ -353,18 +320,33 @@ class TaskManager {
         
         executor.quantity = quantity;
         
+        // Пересчитываем общее выполнение
         const totalCompleted = this.tasks[taskIndex].executors.reduce((sum, e) => sum + (e.quantity || 0), 0);
         this.tasks[taskIndex].completedQuantity = totalCompleted;
         
-        if (executor.quantity >= this.tasks[taskIndex].totalQuantity) {
-            executor.status = 'completed';
+        // Если исполнитель выполнил свою часть, но задача ещё не завершена
+        if (executor.quantity > 0 && executor.status !== 'completed') {
+            executor.status = 'in_progress';
+        }
+        
+        // Проверяем, выполнена ли задача полностью
+        if (totalCompleted >= this.tasks[taskIndex].totalQuantity) {
+            this.tasks[taskIndex].status = 'completed';
+            this.tasks[taskIndex].completedQuantity = this.tasks[taskIndex].totalQuantity;
+            
+            this.tasks[taskIndex].executors.forEach(e => {
+                e.status = 'completed';
+            });
+            
+            this.updateOrderTaskStatus(taskId, 'completed');
+        } else {
+            if (totalCompleted > 0) {
+                this.tasks[taskIndex].status = 'in_progress';
+                this.updateOrderTaskStatus(taskId, 'in_progress');
+            }
         }
         
         this.saveHistoryForDate(this.currentDate);
-        
-        const squareStatus = totalCompleted > 0 ? 'in_progress' : 'pending';
-        this.updateOrderTaskStatus(taskId, squareStatus);
-        
         return true;
     }
     
@@ -378,12 +360,11 @@ class TaskManager {
         
         executor.status = status;
         
-        this.saveHistoryForDate(this.currentDate);
-        
         if (status === 'in_progress' && this.tasks[taskIndex].status !== 'completed') {
             this.updateTaskStatus(taskId, 'in_progress');
         }
         
+        this.saveHistoryForDate(this.currentDate);
         return true;
     }
     
@@ -415,28 +396,33 @@ class TaskManager {
         return this.tasks;
     }
     
+    // Предыдущий день
     prevDay() {
         this.currentDate.setDate(this.currentDate.getDate() - 1);
         this.loadHistoryForDate(this.currentDate);
         return this.tasks;
     }
     
+    // Следующий день
     nextDay() {
         this.currentDate.setDate(this.currentDate.getDate() + 1);
         this.loadHistoryForDate(this.currentDate);
         return this.tasks;
     }
     
+    // Сегодня
     today() {
         this.currentDate = new Date();
         this.loadHistoryForDate(this.currentDate);
         return this.tasks;
     }
     
+    // Форматировать дату
     formatDate(date) {
         return date.toISOString().split('T')[0];
     }
     
+    // Оповестить другие вкладки
     notifyOtherTabs(taskId, status) {
         const event = new CustomEvent('taskStatusChanged', {
             detail: { taskId, status }
