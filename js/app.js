38174be2bc372ccc3474839f9b3b0890d3154826
pavelislamov-gif <pavel.islamov@gmail.com>
2026-03-19 11:37:24 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -802,7 +802,7 @@ function createSiteRow(name, order, siteKey) {
 
     for (let i = 0; i < operationCount; i++) {
         // ===== ЭТУ СТРОКУ НУЖНО ИСПРАВИТЬ =====
-        const taskId = `${order.id}_${siteKey}_${i}`;
+        const taskId = `${order.id}_${siteKey}_0_${i}`;
         // ========================================
         
         const status = order.tasks && order.tasks[taskId] ? order.tasks[taskId] : '';
